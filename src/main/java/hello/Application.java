7df2f5e3425d004fca2d49f8405d7eadbea4e9ee
vidyshhraj55 <vidyashhraj55@gmail.com>
2018-10-30 16:09:00 +0530

    inmemory with CSRF

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	
+	}
+	 @Bean 
+	    ServletWebServerFactory servletWebServerFactory(){
+	    return new TomcatServletWebServerFactory();
+	    }
+}
